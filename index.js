35d871245a065dc8da3515168c8a377775bd2af7
pdLeal <lealpd@outlook.com>
2023-11-17 01:37:50 -0300

    feat: addPhotos function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const lifeBeat = document.querySelector(".hero-img");
-console.log(lifeBeat);
+const main = document.querySelector("main");
+const vejaMaisBtn = document.getElementById("vejaMais-btn");
+const vejaMais = document.querySelector("main > :nth-last-child(2)");
 
 setInterval(() => {
     lifeBeat.classList.add("batida");
@@ -7,3 +9,18 @@ setInterval(() => {
         lifeBeat.classList.remove("batida");
     }, 500);
 }, 3000);
+
+function addPhotos() {
+    const section = document.createElement("section");
+    section.classList.add("galeria");
+
+    for (let i = 0; i < 4; i++) {
+        const img = document.createElement("img");
+        img.classList.add("img-galeria");
+        img.src = "./imgs/ps1.jpg";
+        section.appendChild(img);
+    }
+    main.insertBefore(section, vejaMais);
+}
+
+vejaMaisBtn.addEventListener("click", addPhotos);
